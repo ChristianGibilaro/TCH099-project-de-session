d3441be619486bd06044fc10950269243d74ed6d
ChristianGibilaro <christian.gibilaro.1@ens.etsmtl.ca>
2025-03-25 01:42:57 -0400

    SUPER MERGE ET UNIFICATION GRACE A DESMONSTRATEUR, Unification pas finie, mais suffisante pour la présentation

--- Scripts/animationScript.js
+++ Scripts/animationScript.js
@@ -1,1 +1,26 @@
-//Faudra mettre le script d'animation ici apres, pour l'instant il sont dans le hmtl
+let isMenuOpen = false;
+var r = document.querySelector(':root');
+var rs = getComputedStyle(r);
+var MenuClosedSize = rs.getPropertyValue('--MenuWidthClosed');
+var MenuOpenedSize = rs.getPropertyValue('--MenuWidthOpened');
+var Opacity = rs.getPropertyValue('--MenuOpacityMax');
+
+
+function toggleMenu() {
+
+    if(!isMenuOpen)
+    {
+        isMenuOpen = true;
+        r.style.setProperty( '--MenuWidth', MenuOpenedSize);
+        r.style.setProperty( '--MenuOpacity', Opacity);
+        r.style.setProperty( '--MenuTextOpacity', 1);
+
+    }else{
+        isMenuOpen = false;
+        menu = document.getElementById("menu");
+        r.style.setProperty('--MenuWidth', MenuClosedSize);
+        r.style.setProperty( '--MenuOpacity', 0);
+        r.style.setProperty( '--MenuTextOpacity', 0);
+    }
+
+}
