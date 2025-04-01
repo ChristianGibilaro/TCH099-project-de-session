a58997fd277589da351da1f22692df9e2c6a769b
ChristianGibilaro <christian.gibilaro.1@ens.etsmtl.ca>
2025-04-01 10:08:41 -0400

    Heavy modifications

--- Scripts/ElementCreator.js
+++ Scripts/ElementCreator.js
@@ -1,6 +1,6 @@
 // A fix: les docs des functions et images IDS
-class ElementCreator{
-    
+class ElementCreator {
+
     scrollableListeID = 0;
     imageID = 0;
     tabletitlesID = 0;
@@ -9,7 +9,7 @@ class ElementCreator{
 
     pageName = "";
 
-    constructor (pageName) {
+    constructor(pageName) {
         this.pageName = pageName;
         console.log("Creator js script initialized for page " + pageName);
     }
@@ -18,9 +18,9 @@ class ElementCreator{
     * @param {String}   baseId      the base for the id ex: "scrollableTable"
     * @param {String}   id          id to apply or null for automatic Id generation
     */
-    IdGenerator(baseId,id,index) {
+    IdGenerator(baseId, id, index) {
         if (id != null) {
-            return 'id = "' + id + '"'; ;
+            return 'id = "' + id + '"';;
         } else {
             return 'id = "' + baseId + '#' + index + '"';
         }
@@ -36,46 +36,46 @@ class ElementCreator{
     * @param {String}   classes     apply more classes to the main div ex: "red rotated" or null to apply the defeault class: generatedScrollableTable
     * @param {String}   extraStyle  apply more styles to the main div ex: "margin: 0 auto;"
     */
-    CreateScrollableTable (width,height,titles,elements,id,classes,extraStyle){
+    CreateScrollableTable(width, height, titles, elements, id, classes, extraStyle) {
 
-        
+
         var styleWidth = "";
-        if(width != null){
+        if (width != null) {
             styleWidth = "width: " + width + ";"
-        }else{
+        } else {
             styleWidth = "width: 100%;"
         }
-    
+
         var styleHeight = "";
-        if(height != null){
+        if (height != null) {
             styleHeight = "height: " + height + ";"
-        }else{
+        } else {
             console.warn("height must be defined");
             return document.write("height must be defined");
         }
 
-        if(classes == null){
+        if (classes == null) {
             classes = "generatedScrollableTable";
         }
 
-        if(extraStyle == null){
+        if (extraStyle == null) {
             extraStyle = "";
         }
 
         this.scrollableListeID++;
-        var styleId = this.IdGenerator("ScrollableTable",id,this.scrollableListeID);
+        var styleId = this.IdGenerator("ScrollableTable", id, this.scrollableListeID);
 
-            console.log("ScrollableTable was sucessfully created with " + styleId + " !");
+        console.log("ScrollableTable was sucessfully created with " + styleId + " !");
 
         return '\
-        <div style="' + styleWidth + extraStyle + ' " class="listeScrollable ' + classes + '"' + styleId + '>' +'\
+        <div style="' + styleWidth + extraStyle + ' " class="listeScrollable ' + classes + '"' + styleId + '>' + '\
             <table style>\
                 <thead>' +
-                titles +
-                '</thead>\
-                <tbody  style="' + styleHeight +';">'+
-                elements +
-                '</tbody>\
+            titles +
+            '</thead>\
+                <tbody  style="' + styleHeight + ';">' +
+            elements +
+            '</tbody>\
             </table>\
         </div>';
 
@@ -90,34 +90,34 @@ class ElementCreator{
     * @param {String}   classes     apply more classes to the main div ex: "red rotated" or null to apply the defeault class: generatedImage
     * @param {String}   extraStyle  apply more styles to the main div ex: "margin: 0 auto;"
     */
-    CreateImage (width,height,src,alt,id,classes,extraStyle){
+    CreateImage(width, height, src, alt, id, classes, extraStyle) {
 
         var styleWidth = "";
-        if(width != null){
+        if (width != null) {
             styleWidth = "width: " + width + ";"
-        }else{
+        } else {
             styleWidth = "width: 100%;"
         }
-    
+
         var styleHeight = "";
-        if(height != null){
+        if (height != null) {
             styleHeight = "height: " + height + ";"
-        }else{
+        } else {
             styleHeight = "height: 100%;"
         }
 
-        if(classes == null){
+        if (classes == null) {
             classes = "generatedImage";
         }
 
-        if(extraStyle == null){
+        if (extraStyle == null) {
             extraStyle = "";
         }
 
         this.imageID++;
-        var styleId = this.IdGenerator("Image",id,this.imageID);
+        var styleId = this.IdGenerator("Image", id, this.imageID);
 
-            console.log("Image (w: " + width + " h: " + height + ") was sucessfully created with " + styleId + " !");
+        console.log("Image (w: " + width + " h: " + height + ") was sucessfully created with " + styleId + " !");
 
         return '<img src = "' + src + '"' + ' style="' + styleHeight + styleWidth + extraStyle + '" class="' + classes + '"' + styleId + 'alt="' + alt + '" >'
     }
@@ -132,49 +132,49 @@ class ElementCreator{
     * @param {String}   classes     apply more classes to the main div ex: "red rotated" or null to apply the defeault class: generatedScrollableTable
     * @param {String}   extraStyle  apply more styles to the main div ex: "margin: 0 auto;"
     */
-    CreateTableTitles (titles,titleSize,colNumb,id,classes,extraStyle){
-        
-        if(titleSize != null){
+    CreateTableTitles(titles, titleSize, colNumb, id, classes, extraStyle) {
+
+        if (titleSize != null) {
             var checkWidth = 0;
-            for(var i = 0; i < titleSize.length ; i++){
+            for (var i = 0; i < titleSize.length; i++) {
                 checkWidth += titleSize[i];
             }
-            if(checkWidth != 100){
+            if (checkWidth != 100) {
                 console.warn("titleSize sum of all values must be == 100");
                 return document.write("titleSize sum of all values must be == 100");
             }
         }
 
 
-        if(titles.length == colNumb){
+        if (titles.length == colNumb) {
 
             var outTitles = "";
-            for(var i = 0; i < titles.length; i++){
-                outTitles += '<th style= "width: ' + titleSize[i] + '%;">'+titles[i]+'</th>';
+            for (var i = 0; i < titles.length; i++) {
+                outTitles += '<th style= "width: ' + titleSize[i] + '%;">' + titles[i] + '</th>';
             }
 
-        }else{
+        } else {
             console.warn("title.lenght must be == colnumb");
             return document.write("title.lenght must be == colnumb");
         }
 
-        if(classes == null){
+        if (classes == null) {
             classes = "generatedTableTitles";
         }
 
-        if(extraStyle == null){
+        if (extraStyle == null) {
             extraStyle = "";
         }
 
         this.tableRowID++;
-        var styleId = this.IdGenerator("TableTitles",id,this.tableRowID);
+        var styleId = this.IdGenerator("TableTitles", id, this.tableRowID);
 
-            console.log("TableTitles (col: " + titles.length + ") was sucessfully created with " + styleId + " !");
+        console.log("TableTitles (col: " + titles.length + ") was sucessfully created with " + styleId + " !");
 
         return '\
-        <tr style="' + extraStyle +' " class="' + classes + '"' + styleId + '>' +
-        outTitles +
-        '</tr>';
+        <tr style="' + extraStyle + ' " class="' + classes + '"' + styleId + '>' +
+            outTitles +
+            '</tr>';
     }
 
     /**
@@ -187,92 +187,92 @@ class ElementCreator{
     * @param {String}   classes     apply more classes to the main div ex: "red rotated" or null to apply the defeault class: generatedScrollableTable
     * @param {String}   extraStyle  apply more styles to the main div ex: "margin: 0 auto;"
     */
-    CreateTableRows (elements,elementsType,elementsSize,colNumb,id,classes,extraStyle){
+    CreateTableRows(elements, elementsType, elementsSize, colNumb, id, classes, extraStyle) {
 
-        if(elementsSize != null){
+        if (elementsSize != null) {
             var checkWidth = 0;
-            for(var i = 0; i < elementsSize.length ; i++){
+            for (var i = 0; i < elementsSize.length; i++) {
                 checkWidth += elementsSize[i];
             }
-            if(checkWidth != 100){
+            if (checkWidth != 100) {
                 console.warn("elememtsSize sum of all values must be == 100");
                 return document.write("elememtsSize sum of all values must be == 100");
             }
         }
 
-        if(classes == null){
+        if (classes == null) {
             classes = "generatedTableRows";
         }
 
-        if(extraStyle == null){
+        if (extraStyle == null) {
             extraStyle = "";
         }
 
         this.tabletitlesID++;
-        var styleId = this.IdGenerator("TableRow",id,this.tabletitlesID);
+        var styleId = this.IdGenerator("TableRow", id, this.tabletitlesID);
 
 
         var outRows = "";
         var valid = -1;
 
-        for(var i = 0; i < elements.length; i++){
-            outRows += '<tr style="' + extraStyle +' " class="' + classes + '"' + styleId + '>';
-            if(elements[i].length != colNumb){
+        for (var i = 0; i < elements.length; i++) {
+            outRows += '<tr style="' + extraStyle + ' " class="' + classes + '"' + styleId + '>';
+            if (elements[i].length != colNumb) {
                 valid = i;
                 break;
             }//[["img","100px","100px","image_failed","FillImage","object-position: 1% 40%;"],["txt"],["txt"]];
-            for(var j = 0; j < elements[i].length; j++){
-                switch(elementsType[j][0]){
+            for (var j = 0; j < elements[i].length; j++) {
+                switch (elementsType[j][0]) {
                     case "img":
-                        outRows += '<td style= "width: ' + elementsSize[j] + '%;">' + 
-                        this.CreateImage(elementsType[j][1],elementsType[j][2],elements[j][j],elementsType[j][3],null,elementsType[j][4],elementsType[j][5]) + '</td>';
+                        outRows += '<td style= "width: ' + elementsSize[j] + '%;">' +
+                            this.CreateImage(elementsType[j][1], elementsType[j][2], elements[j][j], elementsType[j][3], null, elementsType[j][4], elementsType[j][5]) + '</td>';
                         break;
                     case "txt":
                         outRows += '<td style= "width: ' + elementsSize[j] + '%;">' + elements[i][j] + '</td>';
                         break;
-                    
+
                 }
             }
             outRows += '</tr>';
         }
 
-        if(valid != -1){
+        if (valid != -1) {
             console.warn("the row #" + valid + " .lenght != colNumb");
             return document.write("the row #" + valid + " .lenght != colNumb");
         }
 
         console.log("Table Rows (row: " + elements.length + " col: " + elements[0].length + ") was sucessfully created with " + styleId + " !");
         return outRows;
-        
+
     }
 
-    SideBySide(width,height,id,classes,extraStyle,...element){
+    SideBySide(width, height, id, classes, extraStyle, ...element) {
 
-        if(width != null){
+        if (width != null) {
             width = "width: " + width + ";"
-        }else{
+        } else {
             width = ""
         }
-    
-        if(height != null){
+
+        if (height != null) {
             height = "height: " + height + ";"
-        }else{
+        } else {
             height = ""
         }
 
-        if(classes == null){
+        if (classes == null) {
             classes = "generatedSideBySide";
         }
 
-        if(extraStyle == null){
+        if (extraStyle == null) {
             extraStyle = "";
         }
 
         this.sideBySideID++;
-        var styleId = this.IdGenerator("SideBySide",id,this.sideBySideID);
+        var styleId = this.IdGenerator("SideBySide", id, this.sideBySideID);
 
-        var outDiv = '<div class = "SideBySide ' + classes +'" style = "' + extraStyle + height + width + '" id="' + styleId + '>'
-        for(var i = 0; i < element.length;i++){
+        var outDiv = '<div class = "SideBySide ' + classes + '" style = "' + extraStyle + height + width + '" id="' + styleId + '">'
+        for (var i = 0; i < element.length; i++) {
             outDiv += element[i];
         }
         outDiv += "</div>";
@@ -281,4 +281,94 @@ class ElementCreator{
 
         return outDiv;
     }
+
+    CreateMenu(head, topOptions, bottomOptions) {
+        var out = '\
+        <table>\
+            <thead id="title">\
+                <th class="icon"><img src="' + head[0] + '"></th>\
+                <th class="logo FillImage"><a href="Main.html"><img src="' + head[1] + '"></a></th>\
+            </thead>\
+            <tbody>';
+
+        for (var i = 0; i < topOptions.length; i++) {
+            out += '<tr class="menuRow">';
+
+            out += '<td class="icon FillImage"> <div>' +
+                this.CreateImage("", "", topOptions[i][0], "loading failed", null,null , null) + '</div> </td>';
+
+            out += '<td class="menuOptions menuButton"><a style= "height: 100%; width:100%; display:table;" href = "' + topOptions[i][2] + '"> ' + topOptions[i][1] + '</a></td>';
+            out += '</tr>';
+        }
+        out += '\
+            </tbody>\
+        </table>\
+        \
+        <div id="bottomMenu">\
+            <table>\
+                <tbody>';
+
+
+        for (var i = 0; i < bottomOptions.length; i++) {
+            out += '<tr class="menuRow">';
+
+            out += '<td class="icon FillImage"> <div>' +
+                this.CreateImage("", "", bottomOptions[i][0], "loading failed", null,null , null) + '</div> </td>';
+
+            out += '<td class="menuOptions menuButton"><a style= "height: 100%; width:100%; display:table;" href = "' + bottomOptions[i][2] + '"> ' + bottomOptions[i][1] + '</a></td>';
+            out += '</tr>';
+        }
+
+        out+='\
+                </tbody>\
+            </table>\
+        </div>'
+
+        console.log("side by Side was sucessfully created with " + " !");
+
+        return out;
+    }
+
+    CreateDesc(descLeft,descRight){
+        return '\
+        <div id="description" class="description">\
+            <div class="left-text">' +
+                descLeft +
+            '</div>\
+            <div class="right-text">' +
+                descRight +
+            '</div>\
+        </div>';
+    }
+
+    CreateHero(hero,logo){
+        return '\
+        <div id="banniere">\
+            <div id="logo">\
+                <img src="' + logo + '" alt="logo">\
+            </div>\
+        <img src="' + hero + '" alt="banniere">\
+        </div>'
+    }
+
+    PutInDiv(child, id, classes, extraStyle){
+        return '<div id="' + id + '" style = "' + extraStyle + '" class = "' + classes + '">' + child + '</div>'
+    }
 }
+
+/*
+  <div id="contenu">
+    <!-- Baniere -->
+
+
+    <!-- Description -->
+    <div id="description" class="description">
+      <div class="left-text">
+        Ceci est-un texte (Gauche)
+      </div>
+      <div class="right-text">
+        Ceci est-un texte (Droite)
+      </div>
+    </div>
+
+  */
